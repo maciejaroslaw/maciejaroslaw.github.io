44f2989b9f665b997101b5165c248fee23515df7
Jaroslavv <maciejaroslaw@gmail.com>
2019-06-23 16:25:44 +0200

    Second commit

--- script/script.js
+++ script/script.js
@@ -1,6 +1,26 @@
+
+const menu = document.querySelector(".menu");
+const contentIntro = document.querySelector(".content__introduction");
+const contentEducation = document.querySelector(".content__education");
+const contentTech = document.querySelector(".content__tech");
+
+
 function menuWrap(){
-    const menu = document.querySelector(".menu");
     menu.classList.toggle('active');
 }
+function wrapp(){
+    // let position = contentEducation.offsetTop - scrollY;
+    // if(scrollY === 0){
+    //     contentEducation.classList.remove('education--active');
+    // }
+    if(scrollY > contentIntro.offsetTop){
+        contentEducation.classList.add('education--active');
+    }
+    if(scrollY > contentEducation.offsetTop){
+        contentTech.classList.add("tech--active");
+    }
+}
 
+window.addEventListener("scroll", wrapp);
 window.addEventListener("click", menuWrap);
+
