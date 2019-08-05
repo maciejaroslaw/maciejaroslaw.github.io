0ebcbd15a5c7cb6571b560c3a3e0a81ed742ad79
Jaroslavv <maciejaroslaw@gmail.com>
2019-08-05 15:29:43 +0200

    working menu,fixed positions ,more clean code

--- portfolio/script/script.js
+++ portfolio/script/script.js
@@ -1,5 +1,21 @@
-$(document).ready(function (){
-    $(".bcg").click(function(){
-        // $("div.menu").slideOut("slow");
-    })
+const menuButton = document.querySelector('.menu__button');
+const menuUl = document.querySelector('.menu ul');
+const menuLi = document.querySelectorAll('li');
+const menu = document.querySelector('.menu');
+
+menuButton.addEventListener('click', ()=>{
+    menuButton.classList.toggle('open');
+    menuUl.classList.toggle('open');
 })
+
+function goTo(position){
+    window.scroll(0, position);
+}
+
+menuLi.forEach(li => {
+    li.addEventListener('click', function(){
+        const target = document.querySelector(`${this.dataset.target}`);
+        const position = target.offsetTop;
+        goTo(position);
+    })
+});
