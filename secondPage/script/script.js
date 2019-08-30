a3e1330460a6aef82e671d6ee8a8551ad91d0593
Jaroslavv <maciejaroslaw@gmail.com>
2019-08-30 14:30:04 +0200

    Mobile_Version_Logic_Done

--- secondPage/script/script.js
+++ secondPage/script/script.js
@@ -1,29 +1,82 @@
 const startButton = document.querySelector('.startGame');
 
 const input = document.querySelector('[name="result"]');
+const resultsButtons = document.querySelectorAll('.result');
 const form = document.querySelector('.dummy');
 const target = document.querySelector('.eq');
 let x;
 let y;
 let s;
+let result;
 
 function getEq(min, max){
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-        x = Math.floor(Math.random() * (max - min) ) + min;
-        y = Math.floor(Math.random() * (max - min) ) + min;
-
-        let num = Math.floor(Math.random() * (4 - 1) ) + 1;
+        
+        let num = Math.floor(Math.random() * (5 - 1) ) + 1;
+        rightResult = Math.floor(Math.random() * (5 - 1) ) + 1;
         switch(num){
-            case 1: s = '+'; break;
-            case 2: s = '-'; break;
-            case 3: s = '*'; break;
-            case 4: s = ':'; break;
+            case 1: 
+                s = '+'; 
+                x = Math.floor(Math.random() * (max - min) ) + min;
+                y = Math.floor(Math.random() * (max - min) ) + min;
+                result = x + y;
+                resultsButtons.forEach(button => {
+                    if(button.classList.contains(`result${rightResult}`)){
+                        button.innerHTML = `${result}`;
+                    }
+                    else{
+                        button.innerHTML = result + (Math.floor(Math.random() * (12 - 1) ) + 1)
+                    }
+                });
+            break;
+            case 2:
+                s = '-'; 
+                x = Math.floor(Math.random() * (max - min) ) + min;
+                y = Math.floor(Math.random() * (max - min) ) + min;
+                if(x<y){[x, y] = [y, x];}
+                result = x - y;
+                resultsButtons.forEach(button => {
+                    if(button.classList.contains(`result${rightResult}`)){
+                        button.innerHTML = `${result}`;
+                    }
+                    else{
+                        button.innerHTML = result + (Math.floor(Math.random() * (12 - 1) ) + 1)
+                    }
+                });
+            break;
+            case 3: 
+                s = '*'; 
+                x = Math.floor(Math.random() * (max - min) ) + min;
+                y = Math.floor(Math.random() * (10 - 1) ) + 1;
+                result = x * y;
+                resultsButtons.forEach(button => {
+                    if(button.classList.contains(`result${rightResult}`)){
+                        button.innerHTML = `${result}`;
+                    }
+                    else{
+                        button.innerHTML = result + (Math.floor(Math.random() * (12 - 1) ) + 1)
+                    }
+                });
+            break;
+            case 4: 
+                s = ':';
+                x = Math.floor(Math.random() * (max - min) ) + min;
+                y = Math.floor(Math.random() * (10 - 1) ) + 1;
+                while(x%y!=0){
+                    x++;
+                }
+                result = x / y;
+                resultsButtons.forEach(button => {
+                    if(button.classList.contains(`result${rightResult}`)){
+                        button.innerHTML = `${result}`;
+                    }
+                    else{
+                        button.innerHTML = result + (Math.floor(Math.random() * (12 - 1) ) + 1)
+                    }
+                });
+            break; 
         }
-        console.log(num);
-        if(x%y!=0){
-            y++;
-        }
-        target.innerHTML = `MOBILE${x}${s}${y}`;
+        target.innerHTML = `${x}${s}${y}`;
     }
 
     else{
@@ -45,19 +98,19 @@ function getEq(min, max){
     }   
 }
 
-function progressBar(){
-    let bar = document.querySelector('.bar');
-    let width = 0;
-    var id = setInterval(()=>{
-        if(width >= 100){
-            clearInterval(id);
-        }
-        else{
-            width = width + 0.1;
-            bar.style.width = `${width}%`;
-        }
-    }, 10)
-}
+// function progressBar(){
+//     let bar = document.querySelector('.bar');
+//     let width = 0;
+//     var id = setInterval(()=>{
+//         if(width >= 100){
+//             clearInterval(id);
+//         }
+//         else{
+//             width = width + 0.1;
+//             bar.style.width = `${width}%`;
+//         }
+//     }, 10)
+// }
 
 startButton.addEventListener('click', ()=>{
     getEq(1, 50);
@@ -69,13 +122,24 @@ startButton.addEventListener('click', ()=>{
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
 
-    result = parseInt(input.value);
-    if(x+y===result){
+    let inputResult = parseInt(input.value);
+    if(x+y===inputResult){
         console.log("Correct!")
         form.reset();
         getEq(1, 50);
     }
 })
+resultsButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        if(button.classList.contains(`result${rightResult}`)){
+            console.log("I AM THE RIGHT ONE");
+            getEq(1, 50);
+        }
+        else{
+            console.log("I AM NOT");
+        }
+    })
+});
 
 
 
